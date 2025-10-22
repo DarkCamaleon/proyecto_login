e5a21dfba824445887d0e42e9e70136435625481
alejandro <avenegasc@unap.cl>
2025-10-22 17:18:41 -0300

    update

---
+++ src/modules/sector_inmobiliario/pages/LoginMain.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="grid-cols-6">
+    <h1>Bienvenido de Nuevo</h1>
+    <p>Nos alegra verte por aqui. Ingresa a tu cuenta para continuar explorando.</p>
+  </header>
+
+  <main class="grid-cols-6">
+    <h2>Iniciar Sesion</h2>
+
+    <form action="#">
+
+      <label for="usuario">Usuario</label>
+      <input type="text" name="nombre_usuario" id="usuario" placeholder="Ingrese Nombre de Usuario">
+      <label for="contraseña">
+        Contraseña
+      </label>
+
+      <input type="password" name="password" id="contraseña" placeholder="Ingrese Contraseña">
+    </form>
+
+    <span>¿Olvidaste tu contraseña?</span>
+    <button>Ingresar</button>
+
+    <p>¿No tienes una cuenta ? <a href="#">Registrate aqui</a></p>
+  </main>
+
+
+</template>
+
+
+
+<script setup lang="ts">
+
+</script>
+
